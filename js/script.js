567006987fb3022bab1aaeb37e98fd271e879cff
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-19 12:37:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// レイドでつなぐ生誕祭
+async function loadData(){
+
+    const response=await fetch("data/current.json");
+
+    const data=await response.json();
+
+    document.getElementById("currentName").textContent=data.currentName;
+
+    document.getElementById("nextName").textContent=data.nextName || "未定";
+
+    document.getElementById("progress").textContent=
+        data.number+" / "+data.total;
+
+}
+
+loadData();
