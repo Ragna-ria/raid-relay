8b011e14d1e50f11555aec48bd91f95aff452822
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-20 23:58:43 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -4,10 +4,8 @@ function createPlayer(channel) {
 
     const playerArea = document.getElementById("playerArea");
 
-    // 前のプレイヤーを削除
     playerArea.innerHTML = "";
 
-    // 新しいプレイヤーを作成
     player = new Twitch.Player("playerArea", {
         width: "100%",
         height: 420,
