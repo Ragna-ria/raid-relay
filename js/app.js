7446651f5db09cad60a8a2209e4c2e734a7a347e
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-21 00:00:50 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 let currentChannel = "";
 
 async function refresh() {
-
     try {
-
         const data = await loadCurrent();
 
         console.log("取得データ:", data);
@@ -11,23 +9,17 @@ async function refresh() {
         updateUI(data);
 
         if (currentChannel !== data.channel) {
-
             console.log(`チャンネル変更: ${currentChannel} → ${data.channel}`);
 
             currentChannel = data.channel;
 
             createPlayer(currentChannel);
-
         }
 
     } catch (error) {
-
         console.error(error);
-
     }
-
 }
 
 refresh();
-
 setInterval(refresh, 5000);
