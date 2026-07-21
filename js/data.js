87278658ee0e9bda1c67aac8b220db49a11810dc
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-21 21:40:58 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,26 @@
 async function loadCurrent() {
 
-    const response = await fetch(
-        `data/current.json?t=${Date.now()}`,
-        {
-            cache: "no-store"
-        }
-    );
+    try {
 
-    return await response.json();
+        const response = await fetch(
+            `data/current.json?t=${Date.now()}`,
+            {
+                cache: "no-store"
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`JSONの取得に失敗しました (${response.status})`);
+        }
+
+        return await response.json();
+
+    } catch (error) {
+
+        console.error("current.json の読み込みエラー", error);
+
+        return null;
+
+    }
 
 }
