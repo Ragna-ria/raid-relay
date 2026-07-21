4c453b8ac3215342838ecd333179c7c728de13ad
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-21 21:49:40 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,25 +1,12 @@
 function updateUI(data) {
 
-    // データ取得失敗時
-    if (!data) {
-
-        document.getElementById("currentName").textContent = "取得失敗";
-
-        document.getElementById("nextName").textContent = "-";
-
-        document.getElementById("progressText").textContent = "-";
-
-        return;
-
-    }
-
     // 現在の走者
     document.getElementById("currentName").textContent =
-        data.currentName;
+        data.currentName || "未定";
 
     // 次の走者
     document.getElementById("nextName").textContent =
-        data.nextName;
+        data.nextName || "未定";
 
     // 進行状況
     document.getElementById("progressText").textContent =
