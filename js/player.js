a532858d7a53939366be5254c0d22d08c0d94e01
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-21 21:42:33 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,19 +2,32 @@ let player = null;
 
 function createPlayer(channel) {
 
+    // channel が空なら何もしない
+    if (!channel) {
+        console.error("チャンネル名が指定されていません。");
+        return;
+    }
+
+    // プレイヤーエリア取得
     const playerArea = document.getElementById("playerArea");
 
-    // プレイヤーを一旦削除
+    // プレイヤーを作り直す
     playerArea.innerHTML = '<div id="twitch-player"></div>';
 
-    // 新しく生成
+    // 新しいプレイヤー生成
     player = new Twitch.Player("twitch-player", {
+
         width: "100%",
-        height: 420,
+        height: 450,
+
         channel: channel,
+
         parent: [
             "ragna-ria.github.io"
         ]
+
     });
+
+    console.log(`プレイヤー作成：${channel}`);
 
 }
