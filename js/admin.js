d7b7332cc6ed37d00a8462851d35c657fb5e774f
Ragna-ria <riasachi-r@salsa.ocn.ne.jp>
2026-07-23 22:23:24 +0900

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,41 @@
+const API="https://あなたのWorkerURL";
+
+load();
+
+async function load(){
+
+const events=await fetch(API+"/events");
+
+const eventList=await events.json();
+
+const select=document.getElementById("eventSelect");
+
+eventList.forEach(event=>{
+
+const option=document.createElement("option");
+
+option.value=event.id;
+
+option.textContent=event.title;
+
+select.appendChild(option);
+
+});
+
+const current=await fetch(API+"/current");
+
+const data=await current.json();
+
+document.getElementById("currentName").value=data.currentName;
+
+document.getElementById("currentChannel").value=data.channel;
+
+document.getElementById("nextName").value=data.nextName;
+
+document.getElementById("nextChannel").value=data.nextChannel;
+
+document.getElementById("number").value=data.number;
+
+document.getElementById("total").value=data.total;
+
+}
